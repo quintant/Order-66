aa12d76cbbb241c9eb4861c8706c8cc05fe681c3
Sigurjón Ingi Jónsson <34385890+quintant@users.noreply.github.com>
2019-03-03 01:32:24 +0000

    Update Order_66.py

--- Order_66.py
+++ Order_66.py
@@ -1,5 +1,6 @@
 import os
 import time
+from pynput import keyboard
 
 
 def order_66():
@@ -7,7 +8,26 @@ def order_66():
     os.system('cls')
     print("BFV.EXE er dead now :'( ")
     time.sleep(4)
-    quit()
 
 
-order_66()
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.ctrl_r:
+        order_66()
+        return False
+
+
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
